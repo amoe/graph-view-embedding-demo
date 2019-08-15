dccc0e8c133cdfa3b1904f123fa9766f12d86101
David Banks <amoebae@gmail.com>
2019-08-15 17:10:21 +0100

    styles, tweakable demo

--- index.js
+++ index.js
@@ -2,11 +2,36 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import {GraphView, GraphViewModule} from 'occubrow-graph-view';
 
+import 'occubrow-graph-view/dist/occubrow-graph-view.css';
+
+const GRAPH_DATA = {
+    content: "Root",
+    id: 42,
+    label: "Blah",
+    strength: null,
+    children: []
+}
+
+function doTweak(e) {
+    console.log("I would tweak something");
+    GRAPH_DATA.content = "Something else";
+
+    GRAPH_DATA.children.push({
+        content: "Leaf",
+        id: 43,
+        label: "Blah3",
+        strength: null,
+        children: []
+    });
+}
+
 function onReady(e) {
     console.log("document is ready");
     console.log("vue is %o", Vue);
     console.log("graphview is %o", GraphView);
     console.log("relevant module is %o", GraphViewModule);
+
+    document.getElementById('tweak').addEventListener('click', doTweak);
 
     Vue.use(Vuex);
 
@@ -24,13 +49,7 @@ function onReady(e) {
         depthOffset: 120,
         textOffset: 22,
         breadth: 360,
-        graphData: {
-            content: "Root",
-            id: 42,
-            label: "Blah",
-            strength: null,
-            children: []
-        },
+        graphData: GRAPH_DATA,
         textContentTemplate: "{{content}}"
     };
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,17 @@
 const path = require('path');
 
 module.exports = {
-  entry: './index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  }
+    entry: './index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    }
 };
